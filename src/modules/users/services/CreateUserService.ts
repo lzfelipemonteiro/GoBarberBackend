8df370fa647b605c47446d90538bef46751efdec
Felipe Monteiro <luizfelipe.p@live.com>
2020-11-13 18:34:04 -0300

    update

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -30,7 +30,7 @@ class CreateUserService {
     const checkUserExists = await this.usersRepository.findByEmail(email)
 
     if (checkUserExists) {
-      throw new AppError('Email addres already used.')
+      throw new AppError('Email address already used.')
     }
 
     const hashedPassword = await this.hashProvider.generateHash(password)
